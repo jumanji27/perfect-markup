fd6983f6e81e400c3116f2418b1eb175f46a0d37
nazz-nazz <niklebdes@gmail.com>
2013-07-18 15:18:06 +0400

    formatting

--- perfect_markup.js
+++ perfect_markup.js
@@ -1,144 +1,126 @@
 // perfectmarkup.js
-// Unknown script for pixel perfect html/css coding
+// Unknown refactoring script for pixel perfect html/css coding
 // https://github.com/nazz-nazz/perfect_markup
 
 window.onload = function() {
 
-var pm = {
+  var pm = {
 
-  initialize: function(img) {
+    initialize: function(img) {
+      var overlay = pm.help.ce("div");
+      var overlayImg = pm.help.ce("img");
 
-    var overlay = pm.help.ce("div");
-    var overlayImg = pm.help.ce("img");
+      pm.help.sa(overlay, "id", "overlay");
+      document.body.appendChild(overlay);
 
-    pm.help.sa(overlay, "id", "overlay");
-    document.body.appendChild(overlay);
+      pm.help.sa(overlayImg, "id", "overlay_img");
 
-    pm.help.sa(overlayImg, "id", "overlay_img");
+      overlay.appendChild(overlayImg);
 
-    overlay.appendChild(overlayImg);
+      if (pm.doDisplay() == "") {
+        pm.doDisplay("none");
+      }
 
-    if (pm.doDisplay() == "") {
-      pm.doDisplay("none");
-    }
+      pm.addGlobalStyle(
+        "#overlay {" +
+        "  display:" + pm.doDisplay() + ";" +
+        "  position:absolute; top:0; left:0; z-index 99; width:100%; opacity:0.5;" +
+        "}" +
+        "#overlay img { display:block; margin:0 auto; }"
+      );
 
-    pm.addGlobalStyle(
-      "#overlay {" +
-      " display: " + pm.doDisplay() + ";" +
-      " position: absolute;" +
-      " top: 0;" +
-      " left: 0;" +
-      " width: 100%;" +
-      " z-index: 99;" +
-      " opacity: 0.5" +
-      "}" +
-      "#overlay img {" +
-      " display: block;" +
-      " margin: 0 auto;" +
-      "}"
-    );
+      var keyEvent = "keypress";
 
-    var keyEvent = "keypress";
+      // Add hotkeys
+      pm.addEvent(document, keyEvent, function (event) {
+        event = event || window.event;
+        var key = event.keyCode || event.which;
 
-    // Add hotkeys
-    pm.addEvent(document, keyEvent, function (event) {
+        // Shift + Space to show / hide overlay
+        if ( event.shiftKey && (key == 32) ) {
+          (pm.doDisplay() == "none") ? pm.doDisplay("block") : pm.doDisplay("none");
 
-      event = event || window.event;
-      var key = event.keyCode || event.which;
+          if (event.preventDefault) {
+            event.preventDefault();
+          }
 
-      // Shift + Space to show / hide overlay
-      if ( event.shiftKey && (key == 32) ) {
-        (pm.doDisplay() == "none") ? pm.doDisplay("block") : pm.doDisplay("none");
+          event.returnValue = false;
 
-        if (event.preventDefault) {
-          event.preventDefault();
+          return true;
         }
 
-        event.returnValue = false;
+        // Ctrl + Enter – resize window to size of layout
+        if ( event.ctrlKey && (key == 13) ) {
+          pm.help.ge("overlay").style.opacity = "1";
+          (pm.doDisplay() == "none") ? pm.doDisplay("none") : pm.doDisplay("block");
+        }
+      });
 
-        return true;
+      pm.setLayoutImage(img, overlayImg);
+    },
+
+    // Crossbrowser addEventListener
+    addEvent: function(obj, event, handler) {
+      if (obj.addEventListener) {
+        obj.addEventListener(event, handler, false);
+      }
+      else {
+        obj.attachEvent("on" + event, handler);
+      }
+    },
+
+    addGlobalStyle: function(css) {
+      var globalStyle = pm.help.ce("style");
+      pm.help.sa(globalStyle, "type", "text/css");
+
+      var cssText = document.createTextNode(css);
+      globalStyle.appendChild(cssText);
+
+      document.getElementsByTagName("head")[0].appendChild(globalStyle);
+    },
+
+    setLayoutImage: function(src, overlayImg) {
+      var img = new Image();
+      var overlayW, overlayH;
+
+      pm.help.sa(overlayImg, "id", "overlay_img");
+
+      pm.addEvent(img, "load", function () {
+        overlayImg.src = src;
+        overlayW = img.width;
+        overlayH = img.height;
+      });
+
+      img.src = src;
+    },
+
+    doDisplay: function(val) {
+      if (val) {
+        overlay.style.display = val;
+        window.name = val;
       }
 
-      // Ctrl + Enter – resize window to size of layout
-      if ( event.ctrlKey && (key == 13) ) {
-        pm.help.ge("overlay").style.opacity = "1";
-        (pm.doDisplay() == "none") ? pm.doDisplay("none") : pm.doDisplay("block");
-      }
-
-    });
-
-    pm.setLayoutImage(img, overlayImg);
-
-  },
-
-  // Crossbrowser addEventListener
-  addEvent: function(obj, event, handler) {
-    if (obj.addEventListener) {
-      obj.addEventListener(event, handler, false);
-    }
-    else {
-      obj.attachEvent("on" + event, handler);
-    }
-  },
-
-  addGlobalStyle: function(css) {
-    var globalStyle = pm.help.ce("style");
-    pm.help.sa(globalStyle, "type", "text/css");
-
-    var cssText = document.createTextNode(css);
-    globalStyle.appendChild(cssText);
-
-    document.getElementsByTagName("head")[0].appendChild(globalStyle);
-  },
-
-  setLayoutImage: function(src, overlayImg) {
-
-    var img = new Image();
-    var overlayW, overlayH;
-
-    pm.help.sa(overlayImg, "id", "overlay_img");
-
-    pm.addEvent(img, "load", function () {
-      overlayImg.src = src;
-      overlayW = img.width;
-      overlayH = img.height;
-    });
-
-    img.src = src;
-
-  },
-
-  doDisplay: function(val) {
-
-    if (val) {
-      overlay.style.display = val;
-      window.name = val;
-    }
-
-    return window.name;
-
-  },
-
-  // window.name storage routine
-  help: {
-
-    ge: function(id) {
-      return document.getElementById(id);
+      return window.name;
     },
 
-    ce: function(tag) {
-      return document.createElement(tag);
-    },
+    // window.name storage routine
+    help: {
+      ge: function(id) {
+        return document.getElementById(id);
+      },
 
-    sa: function(el, attr, val) {
-      el.setAttribute(attr, val);
-    },
+      ce: function(tag) {
+        return document.createElement(tag);
+      },
 
-  }
+      sa: function(el, attr, val) {
+        el.setAttribute(attr, val);
+      },
+    }
 
-};
+  };
 
-pm.initialize("_dev/layout.png");
+  pm.initialize("_dev/layout.png");
 
 };
 
